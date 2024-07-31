ae905e12e4a403e6d77c61ec93e692cba759fab8
Alex Yehorov <al.yehorov@gmail.com>
2024-07-31 09:31:34 +0100

    Added URLSession cache

--- Sources/AsyncImageLoader/NetworkService.swift
+++ Sources/AsyncImageLoader/NetworkService.swift
@@ -4,10 +4,23 @@ import UIKit
 
 public class NetworkService {
     public static let shared = NetworkService()
-    private init() {}
+    
+    private let urlSession: URLSession
+    
+    private init() {
+        let cacheSizeMemory = 50 * 1024 * 1024
+        let cacheSizeDisk = 200 * 1024 * 1024
+        let urlCache = URLCache(memoryCapacity: cacheSizeMemory, diskCapacity: cacheSizeDisk, diskPath: nil)
+        
+        let config = URLSessionConfiguration.default
+        config.requestCachePolicy = .returnCacheDataElseLoad
+        config.urlCache = urlCache
+        
+        self.urlSession = URLSession(configuration: config)
+    }
     
     public func downloadImage(from url: URL) async throws -> UIImage? {
-        let (data, _) = try await URLSession.shared.data(from: url)
+        let (data, _) = try await urlSession.data(from: url)
         return UIImage(data: data)
     }
 }
